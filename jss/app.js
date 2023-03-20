78411959697e4bcef465c5c2e5d4408dc343ba33
Admolgit <zealous890@gmail.com>
2023-03-20 12:35:39 +0100

    animation

--- jss/app.js
+++ jss/app.js
@@ -1,14 +1,13 @@
 // toggle icon menu bar
-let menuBar = document.querySelector('#menuBar');
-let menuClose = document.querySelector('#menuClose');
-let navbar = document.querySelector('.navbar');
+let menuBar = document.querySelector("#menuBar");
+let menuClose = document.querySelector("#menuClose");
+let navbar = document.querySelector(".navbar");
 
 menuBar.onclick = () => {
-  menuBar.classList.toggle('fa-close');
-  navbar.classList.toggle('active');
-  console.log('menuBar')
-}
-
+  menuBar.classList.toggle("fa-close");
+  navbar.classList.toggle("active");
+  console.log("menuBar");
+};
 
 // Change active to current page
 let sections = document.querySelectorAll("section");
@@ -32,9 +31,32 @@ window.onscroll = () => {
   });
   // Sticky navbar
   let header = document.querySelector("header");
-  header.classList.toggle('sticky', window.scrollY > 100);
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-  // Remove toggle and navbar 
+  // Remove toggle and navbar
   // menuBar.classList.toggle('fa-close');
   // navbar.classList.toggle('active');
 };
+
+// Scroll reveal
+ScrollReveal({ 
+  reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.homeContent, .heading', { origin: 'top', });
+ScrollReveal().reveal('.img, .servicesContainer, .portfolioBox, .contactContainer form, .serviceBox3', { origin: 'bottom', });
+ScrollReveal().reveal('.homeContent h1, .aboutImg, .serviceBox1', { origin: 'left', });
+ScrollReveal().reveal('.homeContent p, .aboutContent, .serviceBox2', { origin: 'right', });
+
+
+// Typed text animation
+const typed = new Typed('.multipleText', {
+  strings: ['Full Stack Developer', 'Frontend Developer', 'Backend Developer', 'Youtuber'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
